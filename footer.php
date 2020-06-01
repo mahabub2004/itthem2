8acc4e9b39bdd73c39023e3d060fbf6ef3a65022
mahabub <mahabub2004@gmail.com>
2020-06-01 21:34:23 +0600

    commit added

---
+++ footer.php
@@ -0,0 +1,19 @@
+	<div class="main_wrap footer_bg">
+		<div class="wrap">
+			<footer>
+				<div id="footer">
+					<p>&copy; All Right Reserved By- IT-Bari.com</p>
+				</div>
+				<?php wp_footer(); ?>
+			</footer>			
+		</div>
+	</div>
+
+	<script type="text/javascript">
+    $(window).load(function() {
+        $('#slider').nivoSlider();
+    });
+    </script>	
+	
+	</body>
+</html>
